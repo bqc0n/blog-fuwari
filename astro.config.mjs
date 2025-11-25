26907e68cbee1a4b3c75e1e30da9835de34292c1
bqc0n <me@bqc0n.com>
2025-11-25 12:17:17 +0900

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.bqc0n.com/",
+    site: "https://bqc0n.com/",
     base: "/",
     trailingSlash: "always",
     integrations: [
